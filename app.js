8bec560582ff32a42697f13657848987fac73628
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-13 03:25:56 +0300

    1

---
+++ app.js
@@ -0,0 +1,48 @@
+// let arr2 = [1, 2, 3, "a", "b"];
+// function arrayQosh(arr) {
+//   let arr3 = [];
+//   arr.forEach((el, i) => {
+//     console.log(String(el), i);
+//     arr3.push(typeof el);
+//   });
+//   return arr3;
+// }
+// console.log(arrayQosh(arr2));
+// 73 74 83 84 86 87 94 95 97 99
+// ================= 73-masala
+// let x = +prompt("raqam kiriting");
+// let y = +prompt("raqam kiriting");
+// let z = +prompt("raqam kiriting");
+// function hajm(x, y, z) {
+//   let result = x * y * z;
+//   return result;
+// }
+// console.log(hajm(x, y, z));
+// ================= 74-masala
+// let son1 = 3;
+// let son2 = 8;
+// let arr = [1, 5, 95, 0, 4, 7];
+// function sonlar(son1, son2, arr) {}
+// ================= 83-masala
+// let soz1 = prompt("yozuv kiriting");
+// let soz2 = prompt("yozuv kiriting");
+// function tengmi(soz1, soz2) {
+//   return soz1.toLowerCase() === soz2.toLowerCase();
+// }
+// console.log(tengmi(soz1, soz2));
+// ================= 84-masala
+
+// ================= 86-masala
+// let arr = [2, 5, 3];
+// function sonlar(arr) {}
+// console.log(sonlar(arr));
+
+// ================= 87-masala
+
+// ================= 94-masala
+
+// ================= 95-masala
+
+// ================= 97-masala
+
+// ================= 99-masala
